021772e59131c1a0667450330e23ee8e3308474d
MogamboPuri <magambo@beldex.io>
2026-01-09 13:25:26 +0000

    chore: update platform-specific generated files

--- example_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import cs_beldex_flutter_libs_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CsBeldexFlutterLibsMacosPlugin.register(with: registry.registrar(forPlugin: "CsBeldexFlutterLibsMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
