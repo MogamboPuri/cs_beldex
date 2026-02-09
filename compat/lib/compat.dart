fa0efdf99cb30cb26887a8e6a2cb8ea161d36a98
MogamboPuri <magambo@beldex.io>
2026-02-09 12:07:14 +0000

    Add Beldex compat layer

---
+++ compat/lib/compat.dart
@@ -0,0 +1,12 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'old_cw_beldex/beldex_wallet_utils.dart';
+export 'old_cw_beldex/node.dart';
+export 'old_cw_core/path_for_wallet.dart';
+export 'old_cw_core/sync_status.dart';
+export 'old_cw_core/wallet_credentials.dart';
+export 'old_cw_core/wallet_info.dart';
+export 'old_cw_core/wallet_type.dart';
