885de9327b045df7fb5a0aa1b2e28bac696bba1a
victor-tucci <victor.tucci@beldex.io>
2026-03-04 12:00:13 +0530

    added logs for debug refresh crash

--- cs_beldex/lib/src/wallets/beldex_wallet.dart
+++ cs_beldex/lib/src/wallets/beldex_wallet.dart
@@ -20,7 +20,7 @@ class BeldexWallet extends Wallet {
 
   int? _walletPointer;
   int _getWalletPointer() {
-    Logging.log?.i("Getting wallet pointer for wallet at address ${_walletPointer}");
+    Logging.log?.i("Getting wallet pointer for wallet at address $_walletPointer");
     if (_walletPointer == null) {
       throw Exception(
         "BeldexWallet was closed!",
